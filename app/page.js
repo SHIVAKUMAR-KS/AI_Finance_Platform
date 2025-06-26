7e9ef51dbf3114669c69fb2f98071bd028bb6ec3
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2025-06-26 21:08:29 +0530

    added all important component

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant="destructive" >Welcome to Next.js!</Button>
-    </div>
+      <Button >Welcome to Next.js!</Button>
+    </div>  
   );
 }
